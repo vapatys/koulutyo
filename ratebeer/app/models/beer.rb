f5cb0e2893f5216a5168d071f852c60ea361f196
vapatys <vaino.snicker@helsinki.fi>
2016-01-31 20:55:19 +0200

    2viikko

--- ratebeer/app/models/beer.rb
+++ ratebeer/app/models/beer.rb
@@ -1,5 +1,16 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    
+    d=self.ratings.average(:score)
+    d
+  end
+
+  def to_s
+    "#{self.name} #{self.brewery.name}"
+  end
 end
 
 
--- ratebeer/app/models/brewery.rb
+++ ratebeer/app/models/brewery.rb
@@ -1,4 +1,12 @@
 class Brewery < ActiveRecord::Base
   has_many :beers 
+
+  def print_report
+    puts self.name
+    puts "established at year #{self.year}"
+    puts "number of beers #{self.beers.count}"
+  end
+
+  
 end
 
